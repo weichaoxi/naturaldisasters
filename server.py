469463187ccb8f29ca66bd077a399d050a3d7058
DESKTOP-OQKE1O0\wx <wx45@drexel.edu>
2018-09-09 07:36:48 -0400

    's'

--- server.py
+++ server.py
@@ -1,7 +1,41 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 import csv
+import re
+import pandas as pd
 
 app = Flask(__name__)
+
+@app.route('/findrisk', methods=['GET','POST'])
+def getRisk():
+    if request.method == 'GET':
+        req = str(request)
+        coordinates = re.findall('(?:latitude|longitude)=(-?\d+.\d+)', req)
+        lat = coordinates[0]
+        lng = coordinates[1]
+
+        df = pd.read_csv('data.csv')
+        
+
+        count = 0
+        
+        
+        for index, row in df.iterrows():
+            try:
+                if(abs(float(row['LATITUDE']) - float(lat) ) <= 1 and abs(float(row['LONGITUDE']) - float(lng) <= 1 )):
+                    count += 1
+            except:
+                continue
+
+        message = "That location has had " + str(count) + " hurricanes and other natural disasters in the past 14 years!"
+        return message
+
+    else:
+        return "Couldn't compute"
+
+
+
+        
+        
 
 @app.route('/')
 def index():
--- static/client.js
+++ static/client.js
@@ -1,12 +1,14 @@
 var geocoder, map;
 
 function initialize() {
-    return;
+    console.log('iniializing..');
 }
 
 function getInfo() {
+    console.log('testing this point');
+    
     geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var latlng = new google.maps.LatLng(0, 0);
     var mapOptions = {
       zoom: 10,
       center: latlng
@@ -14,7 +16,7 @@ function getInfo() {
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     var loc = document.getElementById('location').value
-    console.log('location ' + loc)
+    console.log('location: ' + loc)
     
     //geocoder = new google.maps.Geocoder();
     geocoder.geocode({'address':loc}, function(results, status) {
@@ -24,9 +26,29 @@ function getInfo() {
                 map: map,
                 position: results[0].geometry.location
             });
-          } 
+
+
+            $.ajax({
+                type: "GET",
+                url: "/findrisk",
+                data: JSON.stringify({"latitude": results[0].geometry.location.lat(), "longitude": results[0].geometry.location.lng()}),
+                contentType: 'application/json',
+                dataType: "text",
+                success: function(msg) {
+                    console.log('success!! ' + msg + " ...!") 
+                    //console.log('message: ' + msg);
+                    document.getElementById('risk').innerHTML = msg;
+                    //var zipCode = msg.location.zip;
+                    //getWeather(key, zipCode); //call getWeather function using the parsed zip code
+                },
+                error: function(jgXHR, textStatus, errorThrown) {
+                    alert("Error: " + textStatus + " " + errorThrown);
+                }
+            });
+        } 
         else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     })
+    
 }
