36845ddec1ac3ed82422b3137876c2e1ff7c00c2
DESKTOP-OQKE1O0\wx <wx45@drexel.edu>
2018-09-09 01:33:04 -0400

    adding server

---
+++ static/client.js
@@ -0,0 +1,32 @@
+var geocoder, map;
+
+function initialize() {
+    return;
+}
+
+function getInfo() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 10,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var loc = document.getElementById('location').value
+    console.log('location ' + loc)
+    
+    //geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address':loc}, function(results, status) {
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+          } 
+        else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    })
+}
